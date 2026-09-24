cd0b9feceb2d2dbcb808bc3c3c91b30a4142f220
Anjalika Rukshani <anjalikarukshani51@gmail.com>
2026-09-24 15:13:45 +0530

    feat: support updating designation upper level hierarchy

--- app/UseCases/Designation/UpdateDesignationInteractor.php
+++ app/UseCases/Designation/UpdateDesignationInteractor.php
@@ -1,12 +1,28 @@
 <?php
 
 namespace App\UseCases\Designation;
+
 use App\Models\Designation;
+use Illuminate\Support\Facades\DB;
+
 class UpdateDesignationInteractor
 {
-    public function execute(string $id, array $designationData): Designation {
-        $designation = Designation::findOrFail($id);
-        $designation->update($designationData);
-        return $designation->refresh();
+    public function execute(string $id, array $designationData): Designation
+    {
+        return DB::transaction(function () use ($id, $designationData) {
+            $designation = Designation::findOrFail($id);
+            $this->updateHierarchy($designation, $designationData['upper_level'] ?? null);
+            $designation->update($designationData);
+            return $designation->refresh();
+        });
     }
+    private function updateHierarchy(Designation $designation, ?int $newUpperLevel): void {
+        if ($designation->upper_level === $newUpperLevel) {return;}
+        $designationUnderNewUpper = Designation::where('upper_level', $newUpperLevel)->whereKeyNot($designation->id)->first();
+        $designationUnderCurrent = Designation::where('upper_level', $designation->id)->first();
+        $designationUnderNewUpper?->update(['upper_level' => $designation->id,]);
+        $designationUnderCurrent?->update(['upper_level' => $designationUnderNewUpper?->id,]);
+    }
+
 }
+
